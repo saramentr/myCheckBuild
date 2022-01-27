4e556b9bd965970aa8e97616d52b8af0e511fd2e
Sara Mentr <saramentr@gmail.com>
2022-01-27 18:08:15 +0300

    next step create gramars list

--- dev/myLinuxWorkers/checkLanguages.py
+++ dev/myLinuxWorkers/checkLanguages.py
@@ -27,19 +27,26 @@ archivePass = sys.argv[3]
 archiveUrl = sys.argv[4]
 
 folderName = "/tmp/works/"
+folderGitClone = "/tmp/gitClone/"
+folderTmp = "/tmp/tmpW'
+fileTmp = 'tmpW'
+fileSkeepName = "skeepData"
+fileDataName = "loguistDtatResult.csv'
+branchName = sys.argv[0].split('/')[-1].split('.py')[0]
+dictLanguageControlZero = {'objc':0, 'C':0,'CPP':0,'PY':0,'Other':0}
 if os.path.exists(folderName):
     shutil.rmtree(folderName)
 else:
     os.system('mkdir '+folderName)
 os.system('cd '+folderName+' && wget '+archiveUrl+' -O 1.7z > /dev/null 2>&1')
 os.system('cd '+folderName+' && 7z x 1.7z -p'+archivePass)
-commonTable = pd.read_csv(folderName+'commonTable.csv')
+
 
 def down_git_branch(lname,pname,rname,foldrep,branch):
     if os.path.exists(foldrep):
         shutil.rmtree(foldrep)
     os.system("git clone https://"+lname+":"+pname+"@github.com/"+lname+"/"+rname+".git "+foldrep)
-    os.system("cd "+foldrep+"&&git checkout "+branch)
+    os.system("cd "+foldrep+"&&git checkout "+branch+" || git checkout -b "+branch+" clean")
 
 def save_repo_branch_commit(lname,pname,rname,foldrep,branch,commit):
     os.system("cd "+foldrep+"&&git remote remove origin")
@@ -51,5 +58,65 @@ def save_repo_branch_commit(lname,pname,rname,foldrep,branch,commit):
     os.system("cd "+foldrep+"&&git commit -m \""+commit+"\"")
     os.system("cd "+foldrep+"&&git push origin "+branch)
 
+def threadCommit():
+    save_repo_branch_commit(loginName,passName,repoName,folderGitClone,branchName,"threadCommit add")
+    timer = threading.Timer(1800.0, threadCommit)
+    timer.start()
+
+def chechSize():
+    st = os.statvfs('/')
+    ba = (st.f_bavail * st.f_frsize)
+    if ba < 3*3221225472:
+        save_repo_branch_commit(loginName,passName,repoName,folderGitClone,branchName,"chechSize small free space")
+        os._exit(0)
+    else:
+        timer2 = threading.Timer(600.0, chechSize)
+        timer2.start()
+
+def linguistParse(urlP,foldTP,fileTP):
+    if os.path.exists(foldTP):
+        shutil.rmtree(foldTP)
+     os.system("git clone "+i.splitlines()[0]+' '+foldTP)
+    if not os.path.exists(foldTP):
+        return dictLanguageControlZero
+    if os.path.exists(foldTP+fileTP):
+        os.remove(foldTP+fileTP)
+    os.system('github-linguist '+foldTP+' -j >> '+foldTP+fileTP)
+    if not os.path.exists(foldTP+fileTP):
+        return dictLanguageControlZero
+
+    dT = dictLanguageControlZero
+    f = open(foldTP+fileTP)
+    data = json.load(f)
+    for i in data:
+        dT[i] = 
+        print(i)
+
+    f.close()
+commonTable = pd.read_csv(folderName+'commonTable.csv')
+
+down_git_branch(loginName,passName,repoName,folderGitClone,branchName)
+
+fileSkeepData = open(folderGitClone+fileSkeepName,"a+")
+fileSkeepData.seek(0, 0)
+readSkeepData = fileSkeepData.readlines()
+
+resultData = pd.read_csv(folderGitClone+fileDataName)
+
+for i in commonTable['HASH']:
+    if len(commonTable.loc[commonTable['HASH'] == i]['URL']) > 1:
+        for urlForWork in commonTable.loc[commonTable['HASH'] == i]['URL']:
+            dictTmp = linguistParse(urlForWork,folderTmp,fileTmp)
+     elif len(commonTable.loc[commonTable['HASH'] == i]['URL']) < 1:
+        continue
+    elif i.splitlines()[0] in str(readSkeepData) or not i.startswith('https:') or not i.splitlines()[0].endswith('.git'):
+        urlForWork = commonTable.loc[commonTable['HASH'] == i]['URL'][0] 
+        dictTmp = linguistParse(urlForWork,folderTmp,fileTmp)
+        
+    resultData = resultData.append(dictLanguageControl,ignore_index=True)
+    
+    fileSkeepData.write(i)
+    fileSkeepData.flush()
+    os.fsync(fileSkeepData.fileno())
 os.system('github-linguist --help')
 os._exit(0)
